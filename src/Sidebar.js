0e8c470ea8b2d78734878ddfc96cd92f1664ab3b
Yeri <arifyeripratama@gmail.com>
2017-09-06 00:39:40 +0700

    [ui] detail info

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,56 @@
 import React from 'react'
 
+const DetailInfo = () => {
+  return (
+    <div id="detail-info">
+      <div className="detail-info-heading">
+        <h3 className="detail-info-title">Mie Ayam Tumini</h3>
+      </div>
+      <div className="detail-info-thumbnail">
+        <img alt="restaurant" className="img-responsive" src="//via.placeholder.com/500x300" />
+      </div>
+      <ul className="detail-info-menu media-list">
+        <li className="media">
+          <div className="media-left media-bottom">
+            <img className="media-object" src="//via.placeholder.com/100x100" alt="mie ayam" />
+          </div>
+          <div className="media-body">
+            <h4 className="media-heading cut cut-default">Mie Ayam Jumbo</h4>
+            <h4 className="media-heading f24">Rp15.000</h4>
+          </div>
+        </li>
+        <li className="media">
+          <div className="media-left media-bottom">
+            <img className="media-object" src="//via.placeholder.com/100x100" alt="mie ayam" />
+          </div>
+          <div className="media-body">
+            <h4 className="media-heading cut cut-default">Mie Ayam Jumbo</h4>
+            <h4 className="media-heading f24">Rp15.000</h4>
+          </div>
+        </li>
+        <li className="media">
+          <div className="media-left media-bottom">
+            <img className="media-object" src="//via.placeholder.com/100x100" alt="mie ayam" />
+          </div>
+          <div className="media-body">
+            <h4 className="media-heading cut cut-default">Mie Ayam Jumbo</h4>
+            <h4 className="media-heading f24">Rp15.000</h4>
+          </div>
+        </li>
+        <li className="media">
+          <div className="media-left media-bottom">
+            <img className="media-object" src="//via.placeholder.com/100x100" alt="mie ayam" />
+          </div>
+          <div className="media-body">
+            <h4 className="media-heading cut cut-default">Mie Ayam Jumbo</h4>
+            <h4 className="media-heading f24">Rp15.000</h4>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
 const FormAdd = ({ onSubmit }) => {
   return (
     <form id="form-add" onSubmit={onSubmit}>
@@ -71,6 +122,7 @@ const Sidebar = ({ active, onClose }) => {
       </div>
       <div className="panel-body">
         <FormAdd onSubmit={e => e.preventDefault()} />
+        <DetailInfo />
       </div>
     </div>
   )
