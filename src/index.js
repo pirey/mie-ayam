e3b891013278135ed2db524931d2521875625d8f
Yeri <arifyeripratama@gmail.com>
2017-09-07 21:59:55 +0700

    add gh-pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App/App'
 import './style.css'
-// import registerServiceWorker from './registerServiceWorker'
+import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-// registerServiceWorker()
+registerServiceWorker()
