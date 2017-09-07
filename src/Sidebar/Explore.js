512c38dd4a2ed59bdae51ce7740c2de7639bd5ce
Yeri <arifyeripratama@gmail.com>
2017-09-07 13:38:15 +0700

    little tweak for add new location button

--- src/Sidebar/Explore.js
+++ src/Sidebar/Explore.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import locIcon from '../Map/loc.png'
 
 const Explore = ({ onInitSelect, onClose }) => {
   return (
     <div className="panel panel-default">
       <div className="panel-heading">
         <button onClick={onClose} className="close"><span>&times;</span></button>
-        <button className="btn btn-success" onClick={onInitSelect}>
-          <i className="fa fa-plus"></i>
-          &nbsp;Daftarkan Tempat Baru
+        <button className="btn btn-link btn-lg" onClick={onInitSelect}>
+          <span style={{marginRight: 10}}><img style={{width: 24, height: 24}} src={locIcon} alt="daftarkan tempat baru" /></span>
+          Daftarkan Tempat Baru
         </button>
       </div>
     </div>
